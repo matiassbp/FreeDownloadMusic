fa8448975f647e30ac9bfb6037151a065fcd90b4
matiassbp <matiasbenonip@gmail.com>
2023-11-11 01:29:54 -0300

    Structure project

---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import "../Styles/Home.css"
+
+const Home = () => {
+    return (
+        <div class="container">
+            <div class="Header">
+                <div class="UrlArea">
+                    <center><p>UrlArea</p></center>
+                </div>
+            </div>
+
+            <div class="CentralArea">
+                <div class="Central">
+                    <center><p>Central</p></center>
+                </div>
+            </div>
+
+            <div class="Footer">
+                <div class="Footer1">
+                    <center><p>Footer1</p></center>
+                </div>
+                <div class="Footer2">
+                    <center><p>Footer2</p></center>
+                </div>
+                <div class="Footer3">
+                    <center><p>Footer3</p></center>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Home
