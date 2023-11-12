17763e10b8f55b1a7e6f960e32b8b6b96b0cd333
matiassbp <matiasbenonip@gmail.com>
2023-11-11 23:52:00 -0300

    cambios leves

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import { RiPlayCircleLine, RiDownload2Fill } from "react-icons/ri";
 
 const Body = () => {
   return (
     <main className="flex items-center justify-center mt-24">
-      <div className="bg-white rounded-lg shadow-md p-8 transform hover:scale-105 transition-transform duration-300 ease-in-out">
+      <div className="bg-gray-600 rounded-lg shadow-md p-8 transform hover:scale-105 transition-transform duration-300 ease-in-out">
         <img
           src="https://photos.bandsintown.com/thumb/10371902.jpeg" 
           alt="Descripción de la imagen"
-          className="w-52 h-52 rounded-lg mx-auto mb-4 object-cover"
+          className="w-64 h-64 rounded-lg mx-auto mb-4 object-cover shadow-lg"
         />
         <div className="flex justify-center space-x-4">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded focus:outline-none focus:bg-blue-600 hover:bg-blue-600 transform transition-transform duration-300 ease-in-out hover:scale-105">
+          <button className="bg-blue-500 text-gray-800 px-6 py-3 rounded-full focus:outline-none focus:bg-blue-600 hover:bg-blue-600 transform transition-transform duration-300 ease-in-out hover:scale-105 flex items-center">
+            <RiPlayCircleLine className="text-2xl mr-2" />
             Reproducir
           </button>
-          <button className="bg-green-500 text-white px-4 py-2 rounded focus:outline-none focus:bg-green-600 hover:bg-green-600 transform transition-transform duration-300 ease-in-out hover:scale-105">
+          <button className="bg-green-500 text-gray-800 px-6 py-3 rounded-full focus:outline-none  focus:bg-green-600 hover:bg-green-600 transform transition-transform duration-300 ease-in-out hover:scale-105 flex items-center">
+            <RiDownload2Fill className="text-2xl mr-2" />
             Descargar
           </button>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,16 @@ import React from 'react';
 const Header = () => {
   return (
     <header className="text-white p-4">
+      <h1 className="text-4xl font-bold text-center text-gray-800 mb-8 mt-8">Nombre de la App</h1>
       <div className="container mx-auto flex items-center justify-center">
         {/* esto es momentaneo, pero hay que mejorar el margin top. */}
-        <div className="flex items-center" style={{ marginTop: '123px' }}> 
+        
+        <div className="flex items-center"> 
           <input
             type="text"
-            placeholder="Buscar..."
-            className="py-2 px-4 border border-gray-600 rounded-l w-96 focus:outline-none focus:border-teal-500"
-          />
-          <button className="bg-green-500 py-2 px-4 rounded-r hover:bg-green-600 focus:outline-none focus:bg-green-700">
+            placeholder="Ingrese la URL de la canción"
+            className="py-2 px-4 border bg-gray-600 border-gray-600 rounded w-96 focus:outline-none focus:border-cyan-500"/>
+          <button className="bg-cyan-500 py-2 px-4 rounded hover:bg-cyan-600 focus:outline-none focus:bg-cyan-700 ml-3">
             Buscar
           </button>
         </div>
