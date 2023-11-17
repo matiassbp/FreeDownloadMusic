07f64cdf64d0582f18777de37c11e40fd0eee56f
matiassbp <matiasbenonip@gmail.com>
2023-11-17 19:04:03 -0300

    edit component

--- src/components/Body.js
+++ src/components/Body.js
@@ -70,20 +70,20 @@ const Body = () => {
       <main className="flex items-center justify-center mt-24">
 
 
-        
+
       
-      <div className="bg-gray-600 rounded-lg shadow-md p-8 transform hover:scale-105 transition-transform duration-300 ease-in-out">
+      <div className="bg-gray-600 rounded-lg shadow-md p-8 transform hover:scale-105 transition-transform duration-10 ease-in-out">
         <img
           src="https://photos.bandsintown.com/thumb/10371902.jpeg" 
           alt="Descripción de la imagen"
           className="w-64 h-64 rounded-lg mx-auto mb-4 object-cover shadow-lg"
         />
         <div className="flex justify-center space-x-4">
-          <button className="bg-blue-500 text-gray-800 px-6 py-3 rounded-full focus:outline-none focus:bg-blue-600 hover:bg-blue-600 transform transition-transform duration-300 ease-in-out hover:scale-105 flex items-center">
+          <button className="bg-blue-500 text-gray-800 px-6 py-3 rounded-full focus:outline-none focus:bg-blue-600 hover:bg-blue-600 transform transition-transform duration-10 ease-in-out hover:scale-105 flex items-center">
             <RiPlayCircleLine className="text-2xl mr-2" />
             Reproducir
           </button>
-          <button className="bg-green-500 text-gray-800 px-6 py-3 rounded-full focus:outline-none  focus:bg-green-600 hover:bg-green-600 transform transition-transform duration-300 ease-in-out hover:scale-105 flex items-center">
+          <button className="bg-green-500 text-gray-800 px-6 py-3 rounded-full focus:outline-none  focus:bg-green-600 hover:bg-green-600 transform transition-transform duration-10 ease-in-out hover:scale-105 flex items-center">
             <RiDownload2Fill className="text-2xl mr-2" />
             Descargar
           </button>
